e48b34aff950a9d8f7c61718ae097cb7e9655c14
220428174 <83217193+220428174@users.noreply.github.com>
2022-09-15 17:48:10 +1000

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,8 @@ const Header = (props) => {
             <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/login">LOGIN. </a> </button>
             <button name="button" value="OK" type="button">POST.</button>
             <button name="button" value="OK" type="button">CONTACT.</button>
-        
+            
+            <Button onClick={logout}>Logout</Button>
             
             
         </div>
