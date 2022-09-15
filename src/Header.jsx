d5f4a66d2616eb9adb250fc52b75b6d439048a93
220428174 <83217193+220428174@users.noreply.github.com>
2022-09-15 16:34:24 +1000

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,11 +5,11 @@ const Header = (props) => {
     return (
         <div className="header-div">
 
-            <button name="button" value="OK" type="button">HOME</button>
-            <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/register">REGISTER </a> </button>
-            <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/login">LOGIN </a> </button>
-            <button name="button" value="OK" type="button">POST</button>
-            <button name="button" value="OK" type="button">CONTACT</button>
+            <button name="button" value="OK" type="button">HOME.</button>
+            <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/register">REGISTER. </a> </button>
+            <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/login">LOGIN. </a> </button>
+            <button name="button" value="OK" type="button">POST.</button>
+            <button name="button" value="OK" type="button">CONTACT.</button>
             
         </div>
     )
