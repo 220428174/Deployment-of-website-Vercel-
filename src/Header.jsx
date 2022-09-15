6519a0ffeb6db8ac6e3dfb7f30cb2a2940b10af6
220428174 <83217193+220428174@users.noreply.github.com>
2022-09-15 16:04:12 +1000

    Update Header.jsx
    functionality restored for both login and register

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
 
             <button name="button" value="OK" type="button">HOME</button>
             <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/register">REGISTER </a> </button>
-            <button name="button" value="OK" type="button"><a href="http://localhost:3000/login">LOGIN </a> </button>
+            <button name="button" value="OK" type="button"><a href="https://sit-313-task-10-1.vercel.app/login">LOGIN </a> </button>
             <button name="button" value="OK" type="button">POST</button>
             <button name="button" value="OK" type="button">CONTACT</button>
             
