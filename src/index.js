7e8f93b8e76dde1629cc9f3ba52e1539d377617b
220428174 <83217193+220428174@users.noreply.github.com>
2022-09-20 10:06:55 +1000

    Update index.js
    Adding routepath for lougout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
+
 import Login from './login';
 import Register from './regi';
-
+import LogoutPage from './LogoutPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,12 +21,15 @@ root.render(
               <Route path="/" element={<App />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-             
+                
+              <Route path="/logout" element={<LogoutPage />} />
     </Routes>
       
     </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>
 );
+
+
 
 
